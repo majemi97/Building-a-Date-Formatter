db690a1c99df394a9ef7e1541edde08ac8eccf97
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-07 16:57:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,8 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-dateOptionsSelectElement.addEventListener("change", () => {} )
+dateOptionsSelectElement.addEventListener("change", () => {
+switch (dateOptionsSelectElement.value) {
+  
+}
+});
