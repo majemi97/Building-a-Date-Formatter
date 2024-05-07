ba92c476945fe0be30ddd996dc29dbbaa561b9f3
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-07 16:21:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 const hours = date.getHours();
-
 const minutes = date.getMinutes();
+
+
+const formattedDate = `${day}`;
